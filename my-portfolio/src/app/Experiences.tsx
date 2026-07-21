357b5c8e51348863d392f7250e1c281d3028c98d
Quwin <ethantran.et77@gmail.com>
2026-07-20 18:58:12 -0700

    Edited dates

--- my-portfolio/src/app/Experiences.tsx
+++ my-portfolio/src/app/Experiences.tsx
@@ -2,8 +2,8 @@ import Experience from "./Experience"
 export default function Experiences() {
     return (
         <div>
-        <Experience company="Synk" image="/Synk_logo.jpeg" position="Software Engineer Intern" startMonth="" startYear="" endMonth="July" endYear="2026"></Experience>
-        <Experience company="Extern" image="/Extern_logo.png" position="Software Engineer Extern (Pfizer-hosted)" startMonth="" startYear="" endMonth="June" endYear="2026"></Experience>
+        <Experience company="Synk" image="/Synk_logo.jpeg" position="Software Engineer Intern" startMonth="July" startYear="2026" endMonth="July" endYear="2026"></Experience>
+        <Experience company="Extern" image="/Extern_logo.png" position="Software Engineer Extern (Pfizer-hosted)" startMonth="May" startYear="2026" endMonth="June" endYear="2026"></Experience>
         <Experience company="Trinity Technology Group" image="/TTG_logo.png" position="Transportation Security Officer" startMonth="July" startYear="2021" endMonth="June" endYear="2025"></Experience>
         {/* Add further experience here */}
         </div>
